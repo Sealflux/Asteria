262c4de5fd356d15322736634237109ccf24e28b
justanothernormalusername <marcusdeng2019@gmail.com>
2026-08-08 15:33:03 -0400

    Script for walking and idle animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,7 +13,17 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	# Get input direction (Vector2)
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
+
 	# Set velocity and move safely against walls
 	velocity = input_dir * SPEED
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play("walk")
+		
+		# Flips sprite hotizontally based off direction
+		if input_dir.x != 0:
+			$AnimatedSprite2D.flip_h = input_dir.x < 0
+		
+	# else:
+	# 	$AnimatedSprite2D.play("idle")
+		
 	move_and_slide()
