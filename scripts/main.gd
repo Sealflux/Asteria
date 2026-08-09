3588d554c91968a7af95fa6e5e69854a04f79c1b
Thuan Nguyen <thuannguyen5177@gmail.com>
2026-08-08 20:37:40 -0400

    Fixed it

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,13 +2,14 @@ extends Node
 var Survivor_Spawn: PackedScene
 var Lantern_Spawn: PackedScene
 var Current_Survivor
+var DialogueBox
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var Trap = get_node("SimpleTrap")
 	Trap.survivor_died.connect(_on_survivor_died)
-	Trap.spawn_survivor.connect(_on_spawn_survivor)
+	Trap.spawn_survivor.connect(_spawn_Survivor)
 
-func spawn_Survivor(survivor : int, position: Vector2):
+func _spawn_Survivor(survivor : int, position: Vector2):
 	# 2. Instantiate the scene
 	if survivor == 1:
 		Survivor_Spawn = preload("res://scenes/Leader.tscn")
@@ -21,19 +22,18 @@ func spawn_Survivor(survivor : int, position: Vector2):
 	var CurrentSurvivor = Survivor_Spawn.instantiate()
 	CurrentSurvivor.position = position
 	add_child(CurrentSurvivor)
-
-#func spawn_Lantern(position: Vector2):
-	#Lantern_Spawn = preload("res://scenes/Lantern.tscn")
-	#var NewLantern = Lantern_Spawn.instantiate()
-	#NewLantern.position = position
-	#add_child(NewLantern) 
-
-func _on_spawn_survivor(survivor: int, position: Vector2) -> void:
-	spawn_Survivor(survivor, position)
-	
-
-
 func _on_survivor_died(Survivor) -> void:
 	print("Death")
 	Survivor.queue_free()
 	#spawn_Lantern(position)
+func ShowDialogue(position: Vector2):
+	DialogueBox = preload("res://scenes/Dialogue.tscn")
+	var NewDialogueBox = DialogueBox.instantiate()
+	NewDialogueBox.position = Vector2(0,0)
+	add_child(NewDialogueBox)
+	
+	
+
+#func spawn_Lantern(position: Vector2):
+	#Lantern_Spawn = preload("res://scenes/Lantern.tscn")
+	#var NewLantern = Lantern_Spawn.instantiate()
