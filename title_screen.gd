ed405fd2cec937b8e8927c33579a9e96943d75a1
Joseph Cho <infilomarie@MacBook-Pro-41.local>
2026-08-09 09:30:56 -0400

    title screen

--- title_screen.gd
+++ title_screen.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Selector.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/intro.tscn")
 	pass # Replace with function body.
 
 
