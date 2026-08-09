1b3bfa6ed142483259271ece071432b60593b255
Thuan Nguyen <thuannguyen5177@gmail.com>
2026-08-09 08:40:38 -0400

    Selector UI almost done

--- scripts/Selector.gd
+++ scripts/Selector.gd
@@ -3,9 +3,32 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var LeaderBox = get_node("HBoxContainer/LeaderBox")
+	var NavigatorBox = get_node("HBoxContainer/NavigatorBox")
+	var TrapmasterBox = get_node("HBoxContainer/TrapmasterBox")
+	var ScholarBox = get_node("HBoxContainer/ScholarBox")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_leader_box_mouse_entered() -> void:
+	print("Mouse Entered LeaderBox")
+	
+
+
+func _on_navigator_box_mouse_entered() -> void:
+	print("Mouse Entered NavigatorBox")
+
+
+
+func _on_trapmaster_box_mouse_entered() -> void:
+	print("Mouse Entered TrapmasterBox")
+
+
+
+func _on_scholar_box_mouse_entered() -> void:
+	print("Mouse Entered ScholarBox")
