b4cdc6ca370992083bf13a237607026db46e4511
Thuan Nguyen <thuannguyen5177@gmail.com>
2026-08-09 09:01:28 -0400

    Selector Done

--- scripts/Selector.gd
+++ scripts/Selector.gd
@@ -1,12 +1,12 @@
 extends Control
-
+var LeaderBox
+var NavigatorBox
+var TrapmasterBox
+var ScholarBox
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var LeaderBox = get_node("HBoxContainer/LeaderBox")
-	var NavigatorBox = get_node("HBoxContainer/NavigatorBox")
-	var TrapmasterBox = get_node("HBoxContainer/TrapmasterBox")
-	var ScholarBox = get_node("HBoxContainer/ScholarBox")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,18 +17,91 @@ func _process(delta: float) -> void:
 
 func _on_leader_box_mouse_entered() -> void:
 	print("Mouse Entered LeaderBox")
+	LeaderBox = $HBoxContainer/LeaderBox/PanelContainer
 	
+	# Create a new StyleBoxFlat resource
+	var style = StyleBoxFlat.new()
+	# Configure the style
+	style.bg_color = Color(0.196, 0.29, 0.751, 1.0)       # Background color
+	LeaderBox.add_theme_stylebox_override("panel", style)   
+
 
 
 func _on_navigator_box_mouse_entered() -> void:
 	print("Mouse Entered NavigatorBox")
+	NavigatorBox = $HBoxContainer/NavigatorBox/PanelContainer
+	
+	# Create a new StyleBoxFlat resource
+	var style = StyleBoxFlat.new()
+	# Configure the style
+	style.bg_color = Color(0.196, 0.29, 0.751, 1.0)       # Background color
+	NavigatorBox.add_theme_stylebox_override("panel", style)  
 
 
 
 func _on_trapmaster_box_mouse_entered() -> void:
 	print("Mouse Entered TrapmasterBox")
+	TrapmasterBox = $HBoxContainer/TrapmasterBox/PanelContainer
+	
+	# Create a new StyleBoxFlat resource
+	var style = StyleBoxFlat.new()
+	# Configure the style
+	style.bg_color = Color(0.196, 0.29, 0.751, 1.0)       # Background color
+	TrapmasterBox.add_theme_stylebox_override("panel", style)  
 
 
 
 func _on_scholar_box_mouse_entered() -> void:
 	print("Mouse Entered ScholarBox")
+	ScholarBox = $HBoxContainer/ScholarBox/PanelContainer
+	
+	# Create a new StyleBoxFlat resource
+	var style = StyleBoxFlat.new()
+	# Configure the style
+	style.bg_color = Color(0.196, 0.29, 0.751, 1.0)       # Background color
+	ScholarBox.add_theme_stylebox_override("panel", style)  
+
+
+func _on_leader_box_mouse_exited() -> void:
+	print("Mouse Left LeaderBox")
+	LeaderBox = $HBoxContainer/LeaderBox/PanelContainer
+	
+	# Create a new StyleBoxFlat resource
+	var style = StyleBoxFlat.new()
+	# Configure the style
+	style.bg_color = Color(0.0, 0.0, 0.0, 1.0)       # Background color
+	LeaderBox.add_theme_stylebox_override("panel", style)   
+
+
+
+func _on_navigator_box_mouse_exited() -> void:
+	print("Mouse Left NavigatorBox")
+	NavigatorBox = $HBoxContainer/NavigatorBox/PanelContainer
+	
+	# Create a new StyleBoxFlat resource
+	var style = StyleBoxFlat.new()
+	# Configure the style
+	style.bg_color = Color(0.0, 0.0, 0.0, 1.0)       # Background color
+	NavigatorBox.add_theme_stylebox_override("panel", style) 
+
+func _on_trapmaster_box_mouse_exited() -> void:
+	print("Mouse Left TrapmasterBox")
+	TrapmasterBox = $HBoxContainer/TrapmasterBox/PanelContainer
+	
+	# Create a new StyleBoxFlat resource
+	var style = StyleBoxFlat.new()
+	# Configure the style
+	style.bg_color = Color(0.0, 0.0, 0.0, 1.0)       # Background color
+	TrapmasterBox.add_theme_stylebox_override("panel", style)  
+
+
+func _on_scholar_box_mouse_exited() -> void:
+	print("Mouse Left ScholarBox")
+	ScholarBox = $HBoxContainer/ScholarBox/PanelContainer
+	
+	# Create a new StyleBoxFlat resource
+	var style = StyleBoxFlat.new()
+	# Configure the style
+	style.bg_color = Color(0.0, 0.0, 0.0, 1.0)       # Background color
+	ScholarBox.add_theme_stylebox_override("panel", style)  
+	
